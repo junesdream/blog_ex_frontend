422d01e39015273c0a7c4188f50812ef6352001b
June <norisori@gmail.com>
2021-12-01 12:34:32 +0100

    first commit

--- src/pages/Music.js
+++ src/pages/Music.js
@@ -1,11 +1,14 @@
 import React from 'react'
+// import ReactPlayer from "react-player";
+import ReactPlayer from "react-player/youtube";
 
 const Music = () => {
     return (
-        <div>
-            
-        </div>
-    )
+		<div>
+			{/* <ReactPlayer url="https://www.youtube.com/watch?v=ysz5S6PUM-U" /> */}
+			<ReactPlayer url="https://www.youtube.com/watch?v=ysz5S6PUM-U" />
+		</div>
+	);
 }
 
 export default Music
